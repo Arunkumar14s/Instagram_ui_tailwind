cd0c2c5109085ec7018595382d4001200a2a0145
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-01-31 19:09:22 +0530

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  content: [ "./index.html","./dark.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
