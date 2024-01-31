bd0c4cc983347c2c8470ad6b15b59ed06180278a
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-01-31 18:47:13 +0530

    configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}",],
+  content: ["./**/*.{html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
