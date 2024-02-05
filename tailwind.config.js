0f1657480ac9f3aaafc5d23fa5be397976fa415b
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-02-05 18:54:32 +0530

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}"],
+  content: [ "./index.html","./dark.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
