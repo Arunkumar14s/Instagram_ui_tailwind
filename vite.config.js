fa2c925c52bba566720aa3de9f465eac1a27daef
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-02-15 20:08:06 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
-
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve("/Instagram_ui_tailwind/", 'index.html'),
-        nested: resolve("/Instagram_ui_tailwind/", 'dark.html')
-      }
-    }
-  }
+  base: "/Instagram_ui_tailwind/"
 })
