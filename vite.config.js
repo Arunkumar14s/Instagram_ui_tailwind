f7232194c122cf2b0052f119fdd199760d36445c
Arunkumar14s <arunkumarsekar5@gmail.com>
2024-02-16 18:48:28 +0530

    nested

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,22 @@
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   base: "/Instagram_ui_tailwind/"
+// })
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'dark.html'),
+      },
+    },
+  },
+
   base: "/Instagram_ui_tailwind/"
+
 })
